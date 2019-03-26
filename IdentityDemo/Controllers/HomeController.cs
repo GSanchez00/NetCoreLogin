fae43563a4b6922cd02ce65fdc7c8bdd22aac8e6
Gustavo Sanchez <it_gsanchez@lnexternos.com.ar>
2019-03-25 21:40:03 -0300

    Agregar archivos de proyecto.

---
+++ IdentityDemo/Controllers/HomeController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using IdentityDemo.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+
+namespace IdentityDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public HomeController(UserManager<IdentityUser> _userManager, RoleManager<IdentityRole> _roleManager)
+        {
+            userManager = _userManager;
+            roleManager = _roleManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+           if (User.Identity.IsAuthenticated)
+           {
+                //userManager.AddClaimAsync();
+                //Agregar un usuario a un rol
+                //userManager.AddToRoleAsync();
+
+                //userManager.ChangeEmailAsync();
+                //userManager.ChangePhoneNumberAsync();
+                //Te pide que vuelvas a colocar tu password para confirmarlo, por ejemplo al cambiar el password. 
+                //userManager.CheckPasswordAsync();
+                //userManager.CreateAsync() //Crea un usuario
+                //Tambien podemos buscar un usuario por email, id, nombre, etc. 
+                //userManager.FindByEmailAsync
+                //Podemos traer todos los claims de un usario. 
+                //userManager.GetClaimsAsync(user);
+                //Trae todos los usuarios que estan en un role especifico. 
+                //userManager.GetUsersInRoleAsync(role);
+                //Tenemos funciones como remover claim, remover usuario de un role, etc
+
+
+
+                await roleManager.CreateAsync(new IdentityRole("Admin"));
+                var user = await userManager.GetUserAsync(HttpContext.User);
+
+                await userManager.AddToRoleAsync(user, "Admin");
+           }
+
+           return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Superman")]
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Sobre nosotros.";
+            return View();
+        }
+
+        [Authorize(Roles ="Admin")]
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Tu pagina de contacto.";
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ IdentityDemo/Startup.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using IdentityDemo.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IdentityDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddRoles<IdentityRole>()
+                .AddDefaultUI(UIFramework.Bootstrap4)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            //services.AddIdentity<IdentityUser, IdentityRole>(options =>
+            //{
+            //    options.User.RequireUniqueEmail = false;
+            //});
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Lockout.AllowedForNewUsers = true;  //Determina si un usario nuevo puede ser bloqueado. El Default es true
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5); //Determina el tiempo de bloqueo. El default es 5 min
+                options.Lockout.MaxFailedAccessAttempts = 10; //Determina la cantidad de intentos fallidos antes de bloquear al usuario. 
+
+                options.Password.RequiredLength = 6;
+                options.Password.RequireDigit = false;
+                options.Password.RequiredUniqueChars = 3; //Cuantos caracteres unicos requiere tu password (cuantos caracteres distintos). 
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+
+                options.SignIn.RequireConfirmedEmail = false;
+                options.SignIn.RequireConfirmedPhoneNumber = false;
+
+                //options.Tokens.AuthenticatorIssuer = "";
+
+                options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+"; //Caracteres permitidos en el nombre del usuario. 
+                options.User.RequireUniqueEmail = true; //Si el usuario requiere un email unico. 
+            });
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                // Cookie settings
+                options.Cookie.HttpOnly = true;
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+
+                options.LoginPath = "/Identity/Account/Login"; //Si no estas logueado, te redirige aca. 
+                options.AccessDeniedPath = "/Identity/Account/AccessDenied"; //Si no tenes acceso a una ruta, te redirige aca. 
+                options.SlidingExpiration = true;
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
